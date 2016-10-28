c068a42af70566056c5e6a4f3f5242e27af25f15
aberry5621 <aberry5621@gmail.com>
2016-10-27 19:36:31 -0700

    Initialize array

--- COMP130 Homework 6 I Think/main.cpp
+++ COMP130 Homework 6 I Think/main.cpp
@@ -7,9 +7,26 @@
 //
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+
+    cout << "Router Math!\n";
+    
+    // declare a 10 by 10 2d array
+    const int SIZE = 10;
+    int m_arr[][SIZE] = {0};
+    
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (j == 0) {
+                m_arr[i][j] = i + 1;
+            }
+            cout << setw(12) << m_arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
